5ee5cd8558c3a4a648a30bf48b3827954e3c1e01
charlychoplin <charly.choplin14@gmail.com>
2024-02-02 10:36:19 +0100

    init and implement get methods

--- src/main/java/com/practice/f1infosapi/controller/PilotesController.java
+++ src/main/java/com/practice/f1infosapi/controller/PilotesController.java
@@ -17,7 +17,6 @@ import java.util.List;
 public class PilotesController {
     private final PilotesService pilotesService;
     private final PilotesMapper pilotesMapper;
-    private final PilotesRepository pilotesRepository;
 
     // Get all drivers
     @GetMapping("/api/v1/pilotes")
